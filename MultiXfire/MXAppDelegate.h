061c77247c050d3e3d6f3fd7a5b7f418501f5b44
James Addyman <jamesaddyman@me.com>
2012-01-24 20:31:27 +0000

    Initial Commit

---
+++ MultiXfire/MXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MXAppDelegate.h
+//  MultiXfire
+//
+//  Created by James Addyman on 24/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MXAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MultiXfire/MXAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  MXAppDelegate.m
+//  MultiXfire
+//
+//  Created by James Addyman on 24/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MXAppDelegate.h"
+
+@implementation MXAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
